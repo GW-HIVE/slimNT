4906c117da0d804b7ef1822b958b334f3fa44e3d
Sean Kim <33474168+seankim658@users.noreply.github.com>
2025-02-03 14:25:12 -0500

    Fix pathing issue

--- pipeline/5_concat_zip.sh
+++ pipeline/5_concat_zip.sh
@@ -7,7 +7,7 @@ cd "$OUTDIR" || exit 1
 
 # Create missing_fna.txt
 touch genomes/empty_list2.txt
-cat genomes/empty_list.txt empty_list2.txt > missing_fna.txt
+cat genomes/empty_list.txt genomes/empty_list2.txt > missing_fna.txt
 
 # Concatenate and compress
 cat genomes/*.fna > slimNT.db
