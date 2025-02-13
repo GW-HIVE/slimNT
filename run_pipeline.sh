4c7c0e2dd9555e4bda933520b3619ac90209f2b4
Sean Kim <33474168+seankim658@users.noreply.github.com>
2025-02-13 13:09:20 -0500

    Add batch directives

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+#SBATCH -p tiny             # Partition to run in
+#SBATCH -t 3:00:00          # Time limit (adjust as needed)
+#SBATCH --job-name=my_pipeline   # Job name
+#SBATCH --output=job_%j.out   # Standard output log
+#SBATCH --error=job_%j.err    # Standard error log
+
+set -e
+cd /scratch/hivelab/slimNT-sean/slimNT || exit 1
 
 chmod +x ./pipeline/*.sh
+
+echo "Pipeline started at $(date)"
 
 ./pipeline/1_get_ids.sh
 ./pipeline/2_get_genomes.sh
 ./pipeline/3_get_alternate_ids.sh
 ./pipeline/4_get_alternate_genomes.sh
 ./pipeline/5_concat_zip.sh
+
+echo "Pipeline completed at $(date)"
